33b4db18d1da63197c5aea80e5397f3808528e37
carlosmartos13 <carlosmartos04@gmail.com>
2020-02-22 17:37:58 -0300

    avançando com estilização

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const routes = express.Router()
+const instructors = require('./instructors')
 
 routes.get('/', function(req, res){
     return res.redirect("/Instructors")
@@ -7,7 +8,17 @@ routes.get('/', function(req, res){
 
 routes.get('/Instructors', function(req, res){
     return res.render("instructors/index")
+
 })
+
+routes.get('/instructors/create', function(req, res){
+    return res.render("instructors/create")
+})
+
+routes.get('/instructors/:id', instructors.show)
+
+routes.post("/instructors", instructors.post)
+
 routes.get('/members', function(req, res){
     return res.render("members/index")
 })
